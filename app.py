2bd324a916467804d86445dae912fd1d0ef61d72
swejan09 <107758501+swejan09@users.noreply.github.com>
2022-07-17 13:53:39 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,14 @@
+import streamlit as st 
+import joblib 
+
+#load the joblib model 
+model_nb = joblib.load('Rating')
+
+#user input 
+st.title("RATING_PRED")
+ip = st.text_input("Enter your message:")
+
+#predict if the entered message is spam or ham 
+op = model_nb.predict([ip])
+if st.button('PREDICT'):
+  st.title(op[0])  #prints the output as spam or ham  
